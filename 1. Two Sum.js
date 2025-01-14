396bbd7037437eed039aa7a717e39843e3b02bea
crespo <aprilianocrespo27@gmail.com>
2025-01-14 18:15:03 +0800

    Add second solution | Problem 1

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,3 +1,4 @@
+//solution 1
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++){
         for (let j = i + 1; j < nums.length; j++){
@@ -8,3 +9,14 @@ var twoSum = function(nums, target) {
     }
 };
 
+//solution 2
+const twoSum2 = (nums, target) => {
+    let numsMap = new Map()
+    for (let i = 0; i < nums.length; i++){
+        const complement = target - nums[i]
+        if (numsMap.has(complement)){
+            return [numsMap.get(complement),i]
+        }
+        numsMap.set(nums[i], i)
+    }
+}
