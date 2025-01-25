5943507918120ad4f4b3f2d132e4d715eb658e2a
crespo <aprilianocrespo27@gmail.com>
2025-01-25 16:10:44 +0800

    Fix problem 20

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,11 @@
+const longestCommonPrefix = () => {
+    
+}
+
+/*  
+
+    [flower,florest, flying]
+    lcp = flower
+    
+
+*/
--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -1,19 +1,20 @@
-const validParentheses = (s) => {
+var isValid = function(s) {
     const stack = []
     const parentheses = {
         '{' : '}',
         '(' : ')',
-        '[' : ']'
+        '[' : ']',
     }
 
     for (const char of s){
         if (char in parentheses){
             stack.push(char)
         } else {
-            if (char !== parentheses[stack[0]]){
+            if (char !== parentheses[stack.pop()]){
                 return false
             }
         }
     }
     return stack.length === 0
-}
+};
+
