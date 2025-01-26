de022637b2c0877d5517dbf31b44a5cc21a37162
crespo <aprilianocrespo27@gmail.com>
2025-01-27 02:03:18 +0800

    Probelm 14

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -1,11 +1,12 @@
-const longestCommonPrefix = () => {
-    
-}
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+    let prefix = strs[0];
 
-/*  
-
-    [flower,florest, flying]
-    lcp = flower
-    
-
-*/
+    for (let i = 1; i < strs.length; i++) {
+      while (strs[i].indexOf(prefix) !== 0) {
+        prefix = prefix.slice(0, -1)
+        if (prefix === "") return ""
+      }
+    }
+    return prefix;
+};
